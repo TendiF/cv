87cee88d7f3b68a36a05a94b8057af87f4f8de28
Tendi <tendifirmansyah30@gmail.com>
2024-06-16 20:58:14 +0700

    init profile & porto new concept

---
+++ assets/video/ffmpeg_reduce_bitrate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Input video file
+input_file="sosialita_customer.mp4"
+output_file="output.mp4"
+
+# Get the original bitrate
+original_bitrate=$(ffmpeg -i "$input_file" 2>&1 | grep bitrate | awk '{print $6}')
+
+# Calculate 50% of the original bitrate
+half_bitrate=$((original_bitrate / 4))
+
+# Reduce the bitrate by 50%
+ffmpeg -i "$input_file" -b:v "${half_bitrate}k" -c:a copy "$output_file"
